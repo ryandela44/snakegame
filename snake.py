2d8d62c62c9aa4268c149c7116e0f256d639e10c
ryandela44 <ryandela44@outlook.fr>
2022-03-10 21:38:21 -0500

    Initial commit

---
+++ snake.py
@@ -0,0 +1,51 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def move(self):
+        for segment_number in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[segment_number - 1].xcor()
+            new_y = self.segments[segment_number - 1].ycor()
+            self.segments[segment_number].goto(new_x, new_y)
+        self.segments[0].forward(20)
+
+    def add_segment(self, position):
+        segment = Turtle("square")
+        segment.color("yellow")
+        segment.penup()
+        segment.goto(position)
+        self.segments.append(segment)
+
+    def extend_snake(self):
+        self.add_segment(self.segments[-1].position())
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(270)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(180)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(0)
